fd8c9e988bc1fdc9ea6fd89fd71242d372991c51
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-07-07 13:05:39 -0400

    Updates

--- selectPreset.js
+++ selectPreset.js
@@ -2,19 +2,29 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("DOM fully loaded. Setting up filter elements...");
 
     function handleFilterClick(e) {
-    console.log("Filter item clicked"); // Log when an item is clicked
-    var filterId = e.target.getAttribute("data-filter-id"); // Get the filter-preset-id of the clicked element
-    console.log(`Clicked item filter ID: ${filterId}`); // Log the filter ID of the clicked item
-    console.log(`Clicked item class: ${e.target.className}`); // Log the class of the clicked item
-    var targetElement = document.querySelector(`[data-preset-id="${filterId}"]`); // Find the target element in the carousel
+        console.log("Filter item clicked"); // Log when an item is clicked
+        var filterId = e.target.getAttribute("data-filter-id"); // Get the filter-preset-id of the clicked element
+        console.log(`Clicked item filter ID: ${filterId}`); // Log the filter ID of the clicked item
+        console.log(`Clicked item class: ${e.target.className}`); // Log the class of the clicked item
 
-    if (targetElement) {
-        console.log(`Scrolling to element with preset ID: ${filterId}`); // Log before scrolling to the element
-        targetElement.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "center" }); // Scroll to the target element
-        console.log(`Scrolled to element with preset ID: ${filterId}`); // Log after scrolling to the element
-    } else {
-        console.log(`Element with preset ID: ${filterId} not found`); // Log if the element was not found
-    }
+        // Toggle "selected" class on the parent element
+        var parentElement = e.target.parentElement;
+        if (parentElement.classList.contains("selected")) {
+            parentElement.classList.remove("selected");
+            console.log("Removed 'selected' class from parent element.");
+        } else {
+            parentElement.classList.add("selected");
+            console.log("Added 'selected' class to parent element.");
+        }
+
+        var targetElement = document.querySelector(`[data-preset-id="${filterId}"]`); // Find the target element in the carousel
+        if (targetElement) {
+            console.log(`Scrolling to element with preset ID: ${filterId}`); // Log before scrolling to the element
+            targetElement.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "center" }); // Scroll to the target element
+            console.log(`Scrolled to element with preset ID: ${filterId}`); // Log after scrolling to the element
+        } else {
+            console.log(`Element with preset ID: ${filterId} not found`); // Log if the element was not found
+        }
     }
   
     // Attach event listeners to all filter elements
