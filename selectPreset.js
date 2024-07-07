04cabe956f9f440cedcdee56e812ae7951bab4ed
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-07-07 12:23:20 -0400

    Updates

--- selectPreset.js
+++ selectPreset.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Function to handle clicking on filter elements
     function handleFilterClick(e) {
+      console.log("Filter item clicked"); // Log when an item is clicked
       var filterId = e.target.getAttribute("data-filter-id"); // Get the filter-preset-id of the clicked element
+      console.log(`Clicked item filter ID: ${filterId}`); // Log the filter ID of the clicked item
       var targetElement = document.querySelector(`[data-preset-id="${filterId}"]`); // Find the target element in the carousel
   
       if (targetElement) {
+        console.log(`Scrolling to element with preset ID: ${filterId}`); // Log before scrolling to the element
         targetElement.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "center" }); // Scroll to the target element
+        console.log(`Scrolled to element with preset ID: ${filterId}`); // Log after scrolling to the element
+      } else {
+        console.log(`Element with preset ID: ${filterId} not found`); // Log if the element was not found
       }
     }
   
